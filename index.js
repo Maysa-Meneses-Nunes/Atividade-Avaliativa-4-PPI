243de4df812c8cff26e4d20c6ab52b66cd6fe99b
Maysa-Meneses-Nunes <maysanunes764@gmail.com>
2024-11-25 17:29:56 -0300

    finalizado

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 import express from 'express';
 
+import session from 'express-session';
+
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
+app.use(session({
+    secret: 'M1nh4Chav3S3cr3t4',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: false, 
+        httpOnly: true,
+        maxAge: 1000 * 60 * 30 
+    }
+}));
+
+app.use(cookieParser());
 
 app.use(express.urlencoded({ extended: true}));
+
+app.use(express.static('./paginas/publicas'));
 
 const porta = 3000;
 const host = 'localhost';
@@ -67,6 +84,12 @@ function cadastroProdutoView(req, resp) {
 }
 
 function menuView(req, resp){
+    const dataHoraUltimoLogin = req.cookies['dataHoraUltimoLogin'];
+    if (!dataHoraUltimoLogin){
+        dataHoraUltimoLogin='';
+    }
+
+
     resp.send(`
         <html>
             <head>
@@ -76,7 +99,7 @@ function menuView(req, resp){
             <body>
                 <nav class="navbar navbar-expand-lg bg-body-tertiary">
                     <div class="container-fluid">
-                        <a class="navbar-brand" href="#">MENU</a>
+                        <a class="navbar-brand" href="#">HOME</a>
                         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                             <div class="navbar-nav">
                                 <a class="nav-link active" aria-current="page" href="/cadastrarProduto">Cadastrar Produto</a>
@@ -98,6 +121,11 @@ function cadastrarProduto(req, resp){
     const validade   = req.body.validade;
     const estoque      = req.body.estoque;
     const fabricante    = req.body.fabricante;
+
+    const dataHoraUltimoLogin = req.cookies['dataHoraUltimoLogin'];
+    if (!dataHoraUltimoLogin){
+        dataHoraUltimoLogin='';
+    }
  
     if (codigo && descricao && precoC && precoV && validade &&  estoque && fabricante){
        
@@ -269,6 +297,9 @@ function cadastrarProduto(req, resp){
         </div>
         </form>
     </div>
+    <div>
+    <p><span>Seu último acesso foi realizado em ${dataHoraUltimoLogin}</span></p>
+    </div>
     </body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </html> `);
@@ -278,10 +309,67 @@ function cadastrarProduto(req, resp){
 
     resp.end();
 }
+function autenticarUsuario(req, resp){
+    const usuario = req.body.usuario;
+    const senha   = req.body.senha;
 
-app.get('/', menuView);
-app.get('/cadastrarProduto', cadastroProdutoView); 
-app.post('/cadastrarProduto',cadastrarProduto);
+    if (usuario === 'admin' && senha === '1234'){
+       
+        req.session.usuarioLogado = true;
+    
+        resp.cookie('dataHoraUltimoLogin', new Date().toLocaleString(), {maxAge: 1000 * 60 * 60 * 24 * 30, httpOnly: true});
+        resp.redirect('/');
+    }
+    else{
+        resp.send(`
+                    <html>
+                        <head>
+                         <meta charset="utf-8">
+                         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+                               integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+                        </head>
+                        <body>
+                            <div class="container w-25"> 
+                                <div class="alert alert-danger" role="alert">
+                                    Usuário ou senha inválidos!
+                                </div>
+                                <div>
+                                    <a href="/login.html" class="btn btn-primary">Tentar novamente</a>
+                                </div>
+                            </div>
+                        </body>
+                        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+                                integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+                                crossorigin="anonymous">
+                        </script>
+                    </html>
+                  `
+        );
+    }
+}
+
+
+function verificarAutenticacao(req, resp, next){
+    if (req.session.usuarioLogado){
+        next(); 
+    }
+    else
+    {
+        resp.redirect('/login.html');
+    }
+}
+app.get('/login', (req, resp) =>{
+    resp.redirect('/login.html');
+});
+app.get('/logout', (req, resp) => {
+    req.session.destroy(); 
+    resp.redirect('/login.html');
+});
+
+app.post('/login', autenticarUsuario);
+app.get('/',verificarAutenticacao, menuView);
+app.get('/cadastrarProduto',  verificarAutenticacao, cadastroProdutoView); 
+app.post('/cadastrarProduto',verificarAutenticacao,cadastrarProduto);
 
 app.listen(porta, host, () => {
     console.log(`Servidor iniciado e em execução no endereço http://${host}:${porta}/`);
